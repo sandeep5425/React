b25de62f0342903ed1ad0184633a175a08373c1d
Sandeep Madhiri <sandeepsunny4464@gmail.com>
2023-08-21 00:37:26 +0530

    Added signup and sign in Feature

--- leetcode_clone/src/components/PageNavigator.jsx
+++ leetcode_clone/src/components/PageNavigator.jsx
@@ -2,7 +2,7 @@ import '../styling/PageNavigator.css'
 import { PROBLEMS, PROBLEMS_PER_PAGE } from '../../BackendData/mockedData'
 
 function PageNavigator({ getProb }) {
-    
+
     const num_of_pages = Math.ceil(PROBLEMS.length / PROBLEMS_PER_PAGE);
     const pages = Array.from(Array(num_of_pages).keys())
 
